b96a3f21a5c40d3ab5c1f866f02c5698e1a240c6
Maria Julia Furtado <mariajulia12003@gmail.com>
2022-11-16 22:14:59 -0300

    (router)

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import { useState } from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Login from "./pages/login";
 import Historico from './pages/historico';
+import SelectedExpense from './pages/selectedExpense'
+import Home from './pages/home'
+
 
 function App() {
 
@@ -9,6 +12,8 @@ function App() {
     <BrowserRouter>
         <Routes>
            <Route path="/" element={< Login/>}/> 
+           <Route path="/Resgistrado" element={<SelectedExpense/>}/> 
+           <Route path="/home" element={< Home/>}/> 
            <Route path="/historico" element={< Historico/>}/> 
         </Routes>
     </BrowserRouter>
