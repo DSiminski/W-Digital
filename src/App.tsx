1d5035b7bcba53e4adb983365801c668f2f9b8cd
Dayane Siminski <siminskidayane@gmail.com>
2022-11-16 22:54:10 -0300

    expensses

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,8 @@ import { useState } from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Login from "./pages/login";
 import Historico from './pages/historico';
-<<<<<<< HEAD
 import Register from './pages/register';
 import Home from './pages/home';
-=======
-import SelectedExpense from './pages/selectedExpense'
-import Home from './pages/home'
-
->>>>>>> c023fa5d9539c4de77602436425f59abce5ed86e
 
 function App() {
 
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -8,24 +8,13 @@ const Home = ( ) => {
   return (
     <div className="container">
       <Header
-<<<<<<< HEAD
         style={{ backgroundColor: "rgb(57, 71, 203)" }}
         isHome={false}
         title="Home"
-=======
-         style={{ backgroundColor: "rgb(57, 71, 203)" }}
-         isHome={true}
-         title="Welcome to your Wallet!"
->>>>>>> c023fa5d9539c4de77602436425f59abce5ed86e
       />
       <div className="container-home">
         <div className="container-inferior">
-<<<<<<< HEAD
             <div className="valor"></div>
-=======
-            <div className="valor">
-            </div>
->>>>>>> c023fa5d9539c4de77602436425f59abce5ed86e
         </div>
       </div>
       <div className="container-two">
@@ -33,18 +22,11 @@ const Home = ( ) => {
       </div>
       <div className="container-three">
         <Button onClick={() => {alert('clicked');}}>New Expenses</Button>
-<<<<<<< HEAD
       </div>
       <Footer/>
     </div>
      
       
-=======
-        </div>
-
-        <Footer/>
-    </S.Container>
->>>>>>> c023fa5d9539c4de77602436425f59abce5ed86e
   );
 };
 export default Home;
